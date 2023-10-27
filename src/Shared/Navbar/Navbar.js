d8d8dfba7f1a1519fd3eb89fd5ad2d705b80a8dc
Md. Selim Hossain <mrrafi812345@gmail.com>
2023-10-28 01:52:21 +0600

    none

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -10,12 +10,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 ">
-                        <li><Link>Home</Link></li>
-                        <li><Link>About</Link></li>
-                        <li><Link>Appointment</Link></li>
-                        <li><Link>Reviews</Link></li>
-                        <li><Link>Contact Us</Link></li>
-                        <li><Link>Login</Link></li>
+                        
                     </ul>
                 </div>
                 <a className="btn btn-ghost normal-case text-xl font-extrabold">Doctor's House</a>
