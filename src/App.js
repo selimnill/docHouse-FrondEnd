805b7d6708393e09d47154659b45c4962106492e
Md. Selim Hossain <mrrafi812345@gmail.com>
2023-10-27 19:28:36 +0600

    additional components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Shared/Routes/Routes';
-import Main from './Pages/Main/Main';
 
 function App() {
   return (
