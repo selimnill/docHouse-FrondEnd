9255e1ec9b066aac629926ed4714cfd2343bdf55
Md. Selim Hossain <mrrafi812345@gmail.com>
2023-11-02 08:42:45 +0600

    checkout form

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+
 const Payment = () => {
-    const {data} = useLoaderData();
+    const data = useLoaderData();
+    const {treatment, appointmentDate, slot, price} = data;
+
+
+    if(NavigationPreloadManager.state === 'loading')
+    {
+        return <p className='text-xl font-bold text-center'>Loading...</p>
+    }
     return (
         <div>
-            <h2 className="text-3xl text-center font-bold mt-5">Payment</h2>
+            <h2 className="text-3xl text-center font-bold mt-5 mb-8">Payment</h2>
+            <h3 className="text-3xl text-center mb-7">Payment For : <b>{treatment}</b></h3>
+            <h5 className="text-xl text-center">Please pay <b>${price}</b> for your Appointment on {appointmentDate} at {slot}</h5>
+            <div className='w-96 my-12'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm
+                        booking={booking}
+                    />
+                </Elements>
+            </div>
         </div>
     );
 };
